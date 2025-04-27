4c43707ecb22cc9be2ea7704f9b43eed8d23172b
G-Chandu-Balaji <balaji5220771@gmail.com>
2025-04-27 15:30:35 +0530

    git link added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className=" flex justify-center mb-3">
-      <a href="">
+      <a href="https://github.com/G-Chandu-Balaji/To-do_LIst">
         <button className="bg-amber-300 rounded-md hover:scale-[1.1] p-2">
           Git-Hub Link
         </button>
